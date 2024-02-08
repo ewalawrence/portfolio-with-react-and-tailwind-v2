f8e1b6e74cf5b0c31a30172583dac453142a898d
Your Name <ewalawrence1991@gmail.com>
2024-02-08 07:19:36 +0100

    changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -83,7 +83,7 @@ const Portfolio = () => {
                 />
                 <div className="flex items-center justify-center">
                   <button  className="w-full px-6 py-3 m-4 duration-200 hover:scale-105">
-                   Next project loading...
+                   Next project loading... <br />
                    Website with HTML5, JavaScript & SASS
                   </button>
                  
